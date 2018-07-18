bff35d815064902df101c21b8b14916e1c806ee4
asalsfkcom <asd@alsfkh.com>
2018-07-19 11:38:15 +1200

    rounds migration made ,changes to the intentions table were also made

--- migrations/20180719112536_intentions.js
+++ migrations/20180719112536_intentions.js
@@ -11,5 +11,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.createTable('intentions')
+  return knex.schema.dropTable('intentions')
 };
--- migrations/20180719112546_round.js
+++ migrations/20180719112546_round.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('rounds', (table)=> {
+    table.increments('id')
+    table.increments('gameid')
+    table.increments('missionid')
+    table.increments('leaderid')
+    // table.integer('roundNum')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('rounds')
 };
