87058e0d6ee3d59f56d88a822e04c9fab9200312
asalsfkcom <asd@alsfkh.com>
2018-07-19 11:44:24 +1200

    created the migration file for the votes, needs a little work but hey not too shabby

--- migrations/20180719112550_votes.js
+++ migrations/20180719112550_votes.js
@@ -1,10 +1,13 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('votes', (table)=>{
-    
+    table.increments('id')
+    table.increments('roundid') //previously we have used round number do we need to identify this too?
+    table.increments('playerid')
+    table.string('vote')
   })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('votes')
 };
