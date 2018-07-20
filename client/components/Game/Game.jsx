8e927e2f1c9863954e355aab4e3782ee44a1b853
reubenjh <rjharcourt@gmail.com>
2018-07-20 15:06:46 +1200

    second action functioning

--- client/components/Game/Game.jsx
+++ client/components/Game/Game.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import GameBoard from './GameBoard'
 import ReadyButton from './ReadyButton'
+import { getPlayers} from '../../actions/games';
+
 
 // this is the head component for both game and waitingRoom- it decides what to render- based
 // on game start, game won etc!!! 
@@ -15,6 +17,12 @@ class Game extends React.Component {
   }
 
 
+
+  componentDidMount(){
+    console.log("Game component trying to dispatch game request")
+    let currentGameId = 1; //This needs to be dynamic
+    this.props.dispatch(getPlayers(currentGameId))
+  }
 
   startGame() {
   }
