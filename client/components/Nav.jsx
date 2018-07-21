6461863fa4e999f7a8f8fbfd51e737881caf7fb1
reubenjh <rjharcourt@gmail.com>
2018-07-22 09:26:36 +1200

    remove extra whitespace at top of page

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -8,22 +8,26 @@ const buttonStyle = 'button is-medium is-link is-inverted is-outlined'
   return (
     <div className="navbar-menu hero is-small is-info">
       <div className="hero-body">
-        <h1 className="title is-1">Persistence</h1>
+       
 
         {props.auth.isAuthenticated
           ?
             <div className="navbar-start">
-            <Link className={buttonStyle} to="/lobby">Home</Link>
+            <h1 className="title is-1">Persistence</h1>
             <div className="navbar-end">
             <p className="nav-item is-size-5">You are logged in as {props.auth.user.user_name}</p>
             <button style={{marginLeft: '1vw'}} className={buttonStyle} onClick={() => props.dispatch(logoutUser())}>Logout</button>
+            <Link className={buttonStyle} to="/lobby">Home</Link>
             </div>
             </div>
           : <div className="navbar-start">
+            <h1 className="title is-1">Persistence</h1>
+            <div className="navbar-end">
             <Link className={buttonStyle} to="/lobby">Home</Link>
             <Link className={buttonStyle} to="/login">Login</Link>&nbsp;
             <Link className={buttonStyle} to="/register">Register</Link>
-          </div>
+            </div>
+            </div>
         //This is how you do/don't display info based on login
       }
       </div>
