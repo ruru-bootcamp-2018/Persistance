7d350fad7c1d1e3008a721ac2edc8d20f747de13
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-07-21 12:24:44 +1200

    moved currentRound, currentMission and mission_params to be their own keys in state (not keys of currentGame), and added extra keys to currentMission

--- Persistence-example-state.js
+++ Persistence-example-state.js
@@ -11,21 +11,25 @@ const state = {
     games: [ //just the games that are not started or finished
         { id:"", is_finished:"", is_progress:"", timestamp:"", game_name:"", host_id:"" }
     ],
+    currentMission: {
+        id:"", game_id:"", outcome:"", //outcome will be null until mission outcome sent to db and currentmission iterated.
+    mission_num:"", mission_approved: false, /*in DB, mebbe not needed in state?*/   
+    },
+    currentRound: {
+        id:"", mission_id:"", leader_id:"", round_num:""
+    },
+    mission_params: [ //could be one level up?
+        { id:"" /*need this?*/, players_total:"", team_total:"", mission_num:"", fails_needed:"" }
+    ],
     currentGame: {
         game:  { id:"", is_finished:"", is_progress:"", timestamp:"", game_name:"", host_id:"" },
+        gameStage:"nominating" || "voting" || "intentions",
         players: [
             { id:"", user_name:"", display_name:"", img:"", game_id:"", role:"" }
         ],
 
-        currentMission: {
-            id:"", game_id:"", outcome:"", //outcome will be null until mission outcome sent to db and currentmission iterated.
-        },
-        currentRound: {
-            id:"", mission_id:"", leader_id:"", round_num:""
-        },
-        mission_params: [ //could be one level up?
-            { id:"" /*need this?*/, players_total:"", team_total:"", mission_num:"", fails_needed:"" }
-        ],
+
+
         missions: [
         {id:"", game_id:"", outcome:"", rounds:"" [
           {id:"", mission_id:"", leader_id:"", round_num:"",
