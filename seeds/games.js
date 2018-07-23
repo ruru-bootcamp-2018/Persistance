63dfa72c76dffebdd7037572bec66356213e7771
Databaseteam <phoenixjstryker@gmail.com>
2018-07-24 11:21:05 +1200

    remove fake data

--- seeds/games.js
+++ seeds/games.js
@@ -1,12 +1,12 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('games').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('games').insert([
-        {id: 1, is_finished: false , in_progress: true , time_stamp: Date.now(), game_name: 'test game'}
-
-      ]);
-    });
+  // return knex('games').del()
+  //   .then(function () {
+  //     // Inserts seed entries
+  //     return knex('games').insert([
+  //       {id: 1, is_finished: false , in_progress: true , time_stamp: Date.now(), game_name: 'test game'}
+  //
+  //     ]);
+  //   });
 };
