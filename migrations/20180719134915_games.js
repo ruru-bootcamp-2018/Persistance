9f72a7248a806e76c5617520c5478d6e489b6b11
Databaseteam <phoenixjstryker@gmail.com>
2018-07-24 15:05:39 +1200

    timestamp

--- migrations/20180719134915_games.js
+++ migrations/20180719134915_games.js
@@ -4,7 +4,7 @@ return knex.schema.createTable('games', (table) => {
   table.increments('id') //previously we have used round number do we need to identify this too?
   table.boolean('is_finished')
   table.boolean('in_progress')
-  table.integer('time_stamp')
+  table.timestamp('time_stamp')
   table.string('game_name')
   table.integer('host_id')
 })
