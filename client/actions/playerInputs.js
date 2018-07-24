c1e38724fefe61b95c9fd70cbc46defdd3323005
phoenixjessiestryker <phoenixjstryker@gmail.com>
2018-07-24 15:55:23 +1200

    prevent double click on start button

--- client/actions/playerInputs.js
+++ client/actions/playerInputs.js
@@ -26,14 +26,6 @@ export function sendIntention(data){
   return request('post', 'game/intention', data)
 }
 
-export function startGame(game, socket){
+export function startGame(game){
   return request('post', 'game/start', {game})
-    .then((res) => {
-      const gameData = res.body
-      const game_id = game.id
-      socket.emit('updateWaitingRoom', gameData, game_id)
-    })
-    .catch(err => {
-      console.log('Error', err.message)
-    })
 }
