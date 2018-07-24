ee19efcdc42757907c5db910a6e407144b33ac18
Databaseteam <phoenixjstryker@gmail.com>
2018-07-24 15:20:07 +1200

    removed timestamp entirely

--- migrations/20180719134915_games.js
+++ migrations/20180719134915_games.js
@@ -4,7 +4,7 @@ return knex.schema.createTable('games', (table) => {
   table.increments('id') //previously we have used round number do we need to identify this too?
   table.boolean('is_finished')
   table.boolean('in_progress')
-  table.timestamp('time_stamp')
+  //table.timestamp('time_stamp') //biginteger?
   table.string('game_name')
   table.integer('host_id')
 })
