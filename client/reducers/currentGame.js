ff8810d9ff339dce496dff1d95a6040983425f50
RossPin <dorpetz@hotmail.com>
2018-08-24 11:26:12 +1200

    RESET_GAME action sets to initial state

--- client/reducers/currentGame.js
+++ client/reducers/currentGame.js
@@ -14,6 +14,8 @@ export default function currentGame(state = initialState, action) {
     switch(action.type) {
         case 'UPDATE_GAME':
             return action.currentGame
+        case 'RESET_GAME':
+            return initialState
         default:
             return state; 
     }
