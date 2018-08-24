e3c09f09179fb0ee7ac0f0a4814b5c77efcf019a
RossPin <dorpetz@hotmail.com>
2018-08-24 13:32:07 +1200

    use history push

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function Home(props) {
-    document.location = props.auth.isAuthenticated ? "/lobby" : "/login"
+    props.history.push(props.auth.isAuthenticated ? "/lobby" : "/login")
   
   return (
     <div></div>
