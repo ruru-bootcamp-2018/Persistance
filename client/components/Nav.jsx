7d7d48dfa76303bdfdec89b31285a670aa8e47ec
CliffRobinson <Clifford.F.Robinson@gmail.com>
2019-02-12 12:28:18 +1300

    css changes so login buttons aren't whited out on small screens

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -8,44 +8,44 @@ function Nav(props) {
   const buttonStyle = 'button is-medium is-link is-inverted is-outlined homeButton'
   return (
     <div className="navbar-menu hero is-small">
-  <div className="hero-body">
+      <div className="hero-body">
 
-    {props.auth.isAuthenticated
-      ?
-      <div className="navbar-start">
-       <img className="persistancetitle" src="/persistencetitle.png" />
-        <div className="navbar-end">
-          <Tooltip
-            // options
-            position="bottom"
-            trigger="mouseenter"
-            html={(
-              <div className="userTooltip" >
-                <p> Logged in: </p>
-                <p>User: {props.auth.user.user_name} </p>
-                <p>Display: {props.auth.user.display_name ? props.auth.user.display_name : props.auth.user.user_name}</p>
-              </div>
-            )}
-          >
-            <img style={{ borderRadius: "50%", marginRight: '1vw' }} className="image is-64x64" src={props.auth.user.img != " " ? props.auth.user.img : "https://tinyurl.com/y7drmeck"} />
-          </Tooltip>
-          <Link style={{marginTop: '0.4vw'}} className={buttonStyle} to="/lobby">Home</Link>
-          <button style={{ marginLeft: '1vw', marginTop: '0.4vw' }} className={buttonStyle} onClick={() => props.dispatch(logoutUser())}>Logout</button>
-        </div>
-      </div>
-      : <div className="navbar-start">
-         <img className="persistancetitle" src="/persistencetitle.png" />
-        <div className="navbar-end">
-          <Link className={buttonStyle} to="/login">Login</Link>&nbsp;
+        {props.auth.isAuthenticated
+          ?
+          <div className="navbar-start">
+            <img className="persistancetitle" src="/persistencetitle.png" />
+            <div className="navbar-end">
+              <Tooltip
+                // options
+                position="bottom"
+                trigger="mouseenter"
+                html={(
+                  <div className="userTooltip" >
+                    <p> Logged in: </p>
+                    <p>User: {props.auth.user.user_name} </p>
+                    <p>Display: {props.auth.user.display_name ? props.auth.user.display_name : props.auth.user.user_name}</p>
+                  </div>
+                )}
+              >
+                <img style={{ borderRadius: "50%", marginRight: '1vw' }} className="image is-64x64" src={props.auth.user.img != " " ? props.auth.user.img : "https://tinyurl.com/y7drmeck"} />
+              </Tooltip>
+              <Link style={{ marginTop: '0.4vw' }} className={buttonStyle} to="/lobby">Home</Link>
+              <button style={{ marginLeft: '1vw', marginTop: '0.4vw' }} className={buttonStyle} onClick={() => props.dispatch(logoutUser())}>Logout</button>
+            </div>
+          </div>
+          : <div className="navbar-start">
+            <img className="persistancetitle" src="/persistencetitle.png" />
+            <div className="navbar-end">
+              <Link className={buttonStyle} to="/login">Login</Link>&nbsp;
           <Link className={buttonStyle} to="/lobby">Home</Link>
-          <Link className={buttonStyle} to="/register">Register</Link>
-        </div>
+              <Link className={buttonStyle} to="/register">Register</Link>
+            </div>
+          </div>
+          //This is how you do/don't display info based on login
+        }
       </div>
-      //This is how you do/don't display info based on login
-    }
-  </div>
-</div>
-)
+    </div>
+  )
 }
 
 const mapStateToProps = ({ auth }) => {
